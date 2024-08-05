1e41648010d8ec6fe2e97c4fd62e6215d8cc3404
Eman-Almokdad <emanmokdad.101@gmail.com>
2024-08-05 22:00:52 +0200

    up3

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,29 +4,13 @@ import { Link } from 'react-router-dom'
 import { details } from '../product'
 import Slider from './Slider'
 const Projects = () => {
-    
+
     return (
         <div className='Projects space' id='Projects'>
             <h5>My Recent Projects</h5>
             <h2>Portfolio</h2>
             <div className="container portfolio-container">
-<Slider slides={details}/>
-                {/* {details.map((element, index) => {
-                    return (
-                        <div className="portfolio-item" key={index}>
-                            <div className="portfolio-image">
-                                <img src={element.img} alt="" />
-                            </div>
-                            <h3>{element.title} </h3>
-                            <div className="portfolio-cta">
-                                <Link to={`/Projects/${element.id}`} className="btn">Detailes</Link>
-                                <a href={element.demo} className="btn btn-primary">Live Demo</a>
-                            </div>
-                        </div>
-                    )
-                })} */}
-
-
+                <Slider slides={details} />
             </div>
         </div>
     )
