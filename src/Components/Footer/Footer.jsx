2151b03d3d6cd51047c650d50c1b2311d11c449d
Eman-Almokdad <emanmokdad.101@gmail.com>
2024-08-11 22:07:20 +0200

    commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = ({Dark , Light}) => {
       <p>To communicate with me :</p>
       <div className="social">
         <a href=""><i className="fa-brands fa-linkedin-in"></i></a>
-        <a href=""><i className="fa-brands fa-instagram"></i></a>
+        {/* <a href=""><i className="fa-brands fa-instagram"></i></a> */}
         <a href=""><i className="fa-regular fa-envelope"></i></a>
         <a href=""><i className="fa-brands fa-github"></i></a>
         <img src={darkMode ? `${Light}`: `${Dark}`   } alt="" onClick={toggleTheme} className='toggleTheme'/>
