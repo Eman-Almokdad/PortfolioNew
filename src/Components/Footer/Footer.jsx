2c2ccd0930a2de6e7e635ad48fef05b47749cbea
Eman-Almokdad <emanmokdad.101@gmail.com>
2024-08-12 23:43:18 +0200

    update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = ({Dark , Light}) => {
     <div className='footer space'>
       <p>To communicate with me :</p>
       <div className="social">
-        <a href=""><i className="fa-brands fa-linkedin-in"></i></a>
+        <a href="https://www.linkedin.com/in/eman-almokdad-7b4854318/" target='_blank'><i className="fa-brands fa-linkedin-in"></i></a>
         {/* <a href=""><i className="fa-brands fa-instagram"></i></a> */}
-        <a href=""><i className="fa-regular fa-envelope"></i></a>
-        <a href=""><i className="fa-brands fa-github"></i></a>
+        <a href="mailto:eman.a.almokdad.a@gmail.com" target='_blank'><i className="fa-regular fa-envelope"></i></a>
+        <a href="https://github.com/Eman-Almokdad" target='_blank'><i className="fa-brands fa-github"></i></a>
         <img src={darkMode ? `${Light}`: `${Dark}`   } alt="" onClick={toggleTheme} className='toggleTheme'/>
       </div>
     </div>
