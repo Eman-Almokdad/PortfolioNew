cbca3fc6dfb6b855c973cb274bc2eff22a41bc3f
Eman-Almokdad <emanmokdad.101@gmail.com>
2024-08-13 21:49:39 +0200

    commit2

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <div className='container'>
                 <div className='about-me'>
                     <div className='container-img'>
-                        <img src='/img/about.jpg' alt="" />
+                        <img src='/img/about.webp' alt="" />
                     </div>
                 </div>
                 <h4 className='container-text'>
